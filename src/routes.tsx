22157475e6764333c17866478d33da48f6f646b7
rosy <rosyshrestha028@gmail.com>
2022-11-16 13:34:05 -0500

    [refactor] Map routes from array of routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,28 +2,51 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { PAGE_URLS } from './constants/route';
 import LandingPage from './routes/landing';
+import ProductDetailPage from './routes/product';
 import SignInPage from './routes/signin';
 import SignUpPage from './routes/signup';
+
+interface IRoute {
+    path: string;
+    component: JSX.Element;
+    exact: boolean;
+}
+
+const routes: Array<IRoute> = [
+    {
+        path: PAGE_URLS.SIGN_IN,
+        exact: true,
+        component: <SignInPage />,
+    },
+    {
+        path: PAGE_URLS.SIGN_UP,
+        exact: true,
+        component: <SignUpPage />,
+    },
+    {
+        path: PAGE_URLS.HOME,
+        exact: true,
+        component: <LandingPage />,
+    },
+    {
+        path: PAGE_URLS.PRODUCT.DETAIL,
+        exact: true,
+        component: <ProductDetailPage />,
+    },
+];
 
 function Routes() {
     return (
         <>
             <Switch>
-                <Route
-                    path={PAGE_URLS.SIGN_IN}
-                    exact
-                    render={() => <SignInPage />}
-                />
-                <Route
-                    path={PAGE_URLS.SIGN_UP}
-                    exact
-                    render={() => <SignUpPage />}
-                />
-                <Route
-                    path={PAGE_URLS.HOME}
-                    exact
-                    render={(props) => <LandingPage {...props} />}
-                />
+                {routes.map((route) => (
+                    <Route
+                        path={route.path}
+                        exact={route.exact}
+                        render={() => route.component}
+                    />
+                ))}
+
                 <Redirect to={PAGE_URLS.HOME} />
             </Switch>
         </>
