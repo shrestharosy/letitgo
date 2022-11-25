96f1a33919241106ce489f9facf1317f5d10b9aa
rosy <rosyshrestha028@gmail.com>
2022-11-25 02:41:32 -0500

    [feat] Check if user has access to edit the item

--- src/routes/product/edit.tsx
+++ src/routes/product/edit.tsx
@@ -8,11 +8,14 @@ import { useHistory, useParams } from 'react-router-dom';
 import { PAGE_URLS } from 'src/constants/route';
 import { useEffect, useState } from 'react';
 import { MainLoader } from 'src/components/Loader';
+import storageUtilityInstance from 'src/libs/utils/storage.util';
+import { USER } from 'src/constants/storage.constant';
+import { IUserProfile } from 'src/service/user/user.type';
 
 const EditProduct = () => {
     const { showSuccess, showError } = useNotify();
 
-    const { push } = useHistory();
+    const { push, goBack } = useHistory();
     const param: { productId?: string } = useParams();
 
     const [isLoading, setIsLoading] = useState(false);
@@ -37,11 +40,23 @@ const EditProduct = () => {
         }
     }, [param?.productId]);
 
+    useEffect(() => {
+        if (product?.owner_id) {
+            const user = storageUtilityInstance.getItem(USER);
+            if (user) {
+                const parsedUser: IUserProfile = JSON.parse(user);
+                if (parsedUser.id !== product.owner_id) {
+                    showError('Access denied');
+                    goBack();
+                }
+            }
+        }
+    }, [product?.owner_id]);
+
     const onSubmit: SubmitHandler<IModifyProduct> = async (data) => {
         try {
             await productService.updateProduct(product.id, data);
             showSuccess('Item modified');
-            // push(PAGE_URLS.USER.ACCOUNT);
         } catch (error) {
             showError(error.message);
         }
@@ -57,7 +72,7 @@ const EditProduct = () => {
                         </Container>
                     )}
 
-                    {product && (
+                    {!isLoading && product && (
                         <UploadProductForm
                             product={product}
                             submitButtonLabel={'Update'}
