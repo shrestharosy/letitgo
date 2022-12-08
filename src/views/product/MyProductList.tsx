4cdc427aac8a2d891c99d0e8ef571f88e2c0de96
rosy <rosyshrestha028@gmail.com>
2022-12-07 22:50:39 -0500

    [fix] Fix issues in fetching user profile by id

--- src/views/product/MyProductList.tsx
+++ src/views/product/MyProductList.tsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from 'react';
 import { Container, Row } from 'reactstrap';
 import { MainLoader } from 'src/components/Loader';
 import ProductRow from 'src/components/ProductRow';
+import { USER } from 'src/constants/storage.constant';
+import { useNotify } from 'src/context/notify';
+import storageUtilityInstance from 'src/libs/utils/storage.util';
 import { productService } from 'src/service/product';
 import { IProduct } from 'src/service/product/product.type';
 
 const MyProductList = () => {
     const [products, setProducts] = useState<Array<IProduct>>([]);
     const [isLoading, setIsLoading] = useState(false);
+
+    const { showError } = useNotify();
 
     useEffect(() => {
         getProducts();
@@ -16,8 +21,15 @@ const MyProductList = () => {
     const getProducts = async () => {
         setIsLoading(true);
         try {
-            const response = await productService.fetchMyProducts(4);
-            setProducts(response);
+            const user = storageUtilityInstance.getItem(USER) ?? null;
+            if (user) {
+                const response = await productService.fetchMyProducts(
+                    JSON.parse(user).id
+                );
+                setProducts(response);
+            } else {
+                showError('User id not found');
+            }
         } catch (error) {
             console.log(error);
         } finally {
